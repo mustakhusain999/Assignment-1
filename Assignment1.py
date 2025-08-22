3c11f8e9c616cddb277264857113a7895b72e8e2
mustakhusain999 <mustakhusain999@gmail.com>
2025-08-22 22:24:33 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,23 @@
+# Task1: Perform Basic Mathematical Operations
+a=input('Enter First Number: ')
+b=input('Enter Second Number: ')
+a=int(a)
+b=int(b)
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print('Addition=',c)
+print('Subtraction=',d)
+print('Multiplication=',e)
+print('Division=',f)
+
+
+# Task 2: Create a Personalized Greeting
+fn=input('Enter your first name: ')
+ln=input('Enter your last name: ')
+fn=str(fn)
+ln=str(ln)
+s='!'
+r=ln+s
+print('Hello,',fn, r,'welcome to the python program.')
